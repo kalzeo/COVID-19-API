52b10ec7d2ad140a540adf487abd4fd30f8cb41c
Kyle McPherson <ixkalz@gmail.com>
2020-05-01 04:12:30 +0100

    Update ncov.py

--- ncov.py
+++ ncov.py
@@ -5,18 +5,37 @@ Data sources used:
   Data hardcoded in HTML, can be accessed from console.log(window.getAreaStat)
   Updated everyday at 5PM GMT+1 (midnight Beijing)
 """
+
 #!flask/bin/python
 #!bin/bash/python
+from time import gmtime, strftime
+import json
+
 from flask import Flask
 from flask_cors import CORS
-from googletransx import Translator
+from bs4 import BeautifulSoup as BS
+from urllib import request
+
+updated = False
 
 app = Flask(__name__)
 CORS(app)
 
-import json
-from bs4 import BeautifulSoup as BS
-from urllib import request
+locationData = []
+statistics = []
+
+def isEmpty(arr):
+    if(len(arr) == 0):
+        return True
+    return False
+
+def compareSame(temp, compareTo):
+    if(temp is compareTo):
+        return True
+    return False
+
+def turnToJSON(obj):
+    return json.dumps(obj)
 
 def getSource():
     dxySource = request.urlopen("http://ncov.dxy.cn/ncovh5/view/pneumonia")
@@ -24,54 +43,78 @@ def getSource():
     soup.prettify()
     return soup
 
-@app.route('/api/location-data/', methods=['GET'])
+def updateLocationData():
+    soup = getSource()
+    temp = []
+
+    try:
+        dxyWorldwide = json.loads(soup.select("#getListByCountryTypeService2true")[0].text[48:][:-11])
+        for k, v in enumerate(dxyWorldwide):
+            index = dxyWorldwide[k]
+            temp.append(
+                {
+                    "place": index['countryFullName'],
+                    "infected": index['currentConfirmedCount'],
+                    "cured": index['curedCount'] ,
+                    "dead": index['deadCount']
+                })
+
+        if not (compareSame(temp, locationData)):
+            locationData.clear()
+            for k,v in enumerate(temp):
+                locationData.append(v)
+
+    except (IndexError, AttributeError, KeyError):
+        pass
+
+def updateStatistics():
+    soup = getSource()
+    temp = []
+
+    try:
+        stats = json.loads(soup.select("#getStatisticsService")[0].text[36:][:-11])
+        globalStats = stats['globalStatistics']
+        try:
+            temp.append(
+                {"totalCases": globalStats['confirmedCount'],
+                 "curedCases": globalStats['curedCount'],
+                 "totalDead": globalStats['deadCount']
+                 })
+
+            if not(compareSame(temp, statistics)):
+                statistics.clear()
+                statistics.append(*temp)
+            else:
+                pass
+
+        except KeyError:
+            pass
+
+    except (IndexError, AttributeError):
+        pass
+
+#-----------------------------------------------------------------------------------------------------------
+@app.route('/api/locations/', methods=['GET'])
 def getLocationData():
-    soup = getSource()
-    data = []
-    dxyCountries = json.loads(soup.select("#getListByCountryTypeService2")[0].text[44:][:-11])
-    dxyProvinces = json.loads(soup.select("#getAreaStat")[0].text[27:][:-11])
+    if(isEmpty(locationData)):
+        updateLocationData()
+    return turnToJSON(locationData)
 
-    for k, v in enumerate(dxyCountries):
-        translate = Translator()
-        countryName = translate.translate(v['provinceName']).text
-        data.append(
-            {"place": countryName,
-             "infected": v['confirmedCount'],
-             "cured": v['curedCount'],
-             "dead": v['deadCount'],
-             "type": "Country"
-             })
-
-    for k, v in enumerate(dxyProvinces):
-        translator = Translator()
-        name = translator.translate(v['provinceName']).text
-        data.append(
-            {"place": name,
-             "infected": v['confirmedCount'],
-             "cured": v['curedCount'],
-             "dead": v['deadCount'],
-             "type": "Province"
-             })
-
-    return json.dumps(data)
-
-@app.route('/api/overall-numbers/', methods=['GET'])
+@app.route('/api/stats/', methods=['GET'])
 def getOverallNumbers():
-    soup = getSource()
-    data = []
-    numbers = json.loads(soup.select("#getStatisticsService")[0].text[36:][:-11])
-    data.append({"totalCases": numbers['confirmedCount'], "suspectedCases": numbers['suspectedCount'], "curedCases": numbers['curedCount'],
-                     "severeCases": numbers['seriousCount'], "totalDead": numbers['deadCount']})
+    if(isEmpty(statistics)):
+        updateStatistics()
 
-    return json.dumps(data)
+    return turnToJSON(statistics)
+
 
 @app.route('/')
 def main():
-    endpoints = f"<h1><u>API Endpoints</u></h1>Location Data - <span style='color:red'>/api/location-data/</span>" \
-                f"<br>Overall Numbers - <span style='color:red'>/api/overall-numbers/</span>"
+    endpoints = f"<h1><u>API Endpoints</u></h1>Location Data - <span style='color:red'>/api/locations/</span>" \
+                f"<br>Overall Statistics - <span style='color:red'>/api/stats/</span>"
 
     howTo = f"<br><br><h1><u>How To Use</u></h1>Add the endpoint to the end of the URL - <span style='color:red'>https://kalzeo.pythonanywhere.com/[ENDPOINT]</span>"
     return endpoints + howTo
 
 if __name__ == '__main__':
-    app.run()
+    app.run(threaded=True)
